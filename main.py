837aa8f98122694b7721f07545a5976d8cb384a2
Mirela Djordjevic <mireladjordjevic@gmail.com>
2026-08-18 11:52:23 +0200

    Update products and inventory display

--- main.py
+++ main.py
@@ -5,21 +5,21 @@
 from product import Product 
 from product_manager import ProductManager
 
-p11 = Product("laptop", 1200, 2)
-p22 = Product("cable", 20, 5)
+p1 = Product("laptop", 1200, 2)
+p2 = Product("cable", 20, 5)
 
-p33 = Product("headphones", 130, 3)
-p44 = Product("mic", 35, 2)
+p3 = Product("headphones", 130, 3)
+p4 = Product("mic", 35, 2)
 m1 = ProductManager([p11,p22])
 
-print(f"Product Manger is : {m1}")
+#print(f"Product Manger is : {m1}")
 
-m1.add_product(p33)
+m1.add_product(p3)
 
 print(f"Ukupna cena: {m1.total_price()}")
 
 m1.remove_product("Laptop") 
-print(f"Product Manager {m1}")
-print(f"Ukupna cen: {m1.total_price()}")
+#print(f"Product Manager {m1}")
+print(f"Ukupna cena: {m1.total_price()}")
 
 
