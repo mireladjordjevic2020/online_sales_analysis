5c65a85ec6acf2deffed257b8dc6ac43afbc5032
Mirela Djordjevic <mireladjordjevic@gmail.com>
2026-08-18 12:02:20 +0200

    Update products and inventory display

--- main.py
+++ main.py
@@ -6,27 +6,23 @@ from cart import Cart
 from product import Product 
 from product_manager import ProductManager
 
-p1 = Product("laptop", 1200, 2)
-p2 = Product("cable", 20, 5)
+p1a = Product("laptop", 1200, 2)
+p2a = Product("cable", 20, 5)
 
-p3 = Product("headphones", 130, 3)
-p4 = Product("mic", 35, 2)
-m1 = ProductManager([p1,p2])
+p3a = Product("headphones", 130, 3)
+p4a = Product("mic", 35, 2)
+m1 = ProductManager([p1a,p2a])
 
-#print(f"Product Manger is : {m1}")
 
-m1.add_product(p3)
+m1.add_product(p3a)
 
-print(f"Ukupna cena: {m1.total_price()}")
 
 m1.remove_product("Laptop") 
-#print(f"Product Manager {m1}")
-print(f"Ukupna cena: {m1.total_price()}")
 
 print(f"Product Manager after removing laptop: {m1}")
 print(f"Ukupna cena: {m1.total_price()}")
 
-m1.add_product(p4)
+m1.add_product(p4a)
 selected_products = random.sample(m1.products, 3)
 cart = Cart(selected_products)
 print(f"Cart : {cart}")
